b0db08562a61695633b9243b4ffd5ae70040e85f
Weikai Lin <wl572@cornell.edu>
2018-05-12 14:44:26 -0400

    initClient

---
+++ weikai/test_setup.js
@@ -0,0 +1,28 @@
+
+function initClient(identifier, keyId){
+  var identifier = "CS.6431";
+  var keyId = 6431;
+  var store = new SignalProtocolStore();
+  var KeyHelper = libsignal.KeyHelper;
+  var registrationId = KeyHelper.generateRegistrationId();
+  var ret = {};
+  ret.keyId = keyId;
+  ret.identifier = identifier;
+  var identityKeyPair;
+
+  KeyHelper.generateIdentityKeyPair().then(function(identityKP) { 
+    ret.identityKeyPair = identityKP;
+    // console.log(ret.identityKeyPair);
+    store.saveIdentity(identifier, identityKP.pubKey);
+  }).then(function(){
+    return KeyHelper.generatePreKey(keyId);
+  }).then(function(preKey){
+    store.storePreKey(preKey.keyId, preKey.keyPair);
+  }).then(function(){
+    return KeyHelper.generateSignedPreKey(ret.identityKeyPair, keyId);
+  }).then(function(signedPreKey){
+    store.storeSignedPreKey(signedPreKey.keyId, signedPreKey.keyPair);
+  });
+  ret.store = store;
+  return ret;
+}
