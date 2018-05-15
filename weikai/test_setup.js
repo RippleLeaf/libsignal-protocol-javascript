2c2c599e90e02e5df6c5e0f43aef827f0db617bc
wklin <wklin@cs.cornell.edu>
2018-05-15 00:45:42 -0400

    reportAbuse

--- weikai/test_setup.js
+++ weikai/test_setup.js
@@ -142,6 +142,17 @@ function sendMessage(sender, rcver, plaintext) {
   }
 }
 
+function _pushHistory(useProfile, contact, evidence, mac) {
+  if(useProfile.history == undefined){
+    useProfile.history = {};
+  }
+  if(useProfile.history[contact] == undefined){
+    useProfile.history[contact] = [];
+  }
+  useProfile.history[contact].push({evidence: evidence, mac: mac});
+}
+
+
 //-----------------------------------------------
 // Receive encrypted message. Build a new session if not exist.
 // Return Promise of a plaintext.
@@ -154,13 +165,19 @@ function receiveMessage(sender, rcver, ciphertext) {
   if(rcver.handShake[sender.name] == undefined){
     // console.log('newDecrypt');
     rcver.handShake[sender.name] = true;
-    return newDecrypt(sender, rcver, ciphertext);
+    return newDecrypt(sender, rcver, ciphertext).then(function (evidence) {
+        _pushHistory(rcver, sender.name, evidence, ciphertext.mac);
+        return evidence.body;
+    });
   }
   else{
     rcver.handShake[sender.name] = true;
     // console.log('doDecrypt');
 
-    return doDecrypt(sender, rcver, ciphertext);
+    return doDecrypt(sender, rcver, ciphertext).then(function (evidence) {
+        _pushHistory(rcver, sender.name, evidence, ciphertext.mac);
+        return evidence.body;
+    });
   }
 }
 
@@ -180,29 +197,58 @@ function MessengerServer() {
     var msgLen = buffer.byteLength;
     return buffer.slice(msgLen - 32);
   };
+
+  this._verifyMAC = function(data, key, mac, length) {
+      return this._calcHmac(key, data).then(function(calculated_mac) {
+          if (mac.byteLength != length  || calculated_mac.byteLength < length) {
+              throw new Error("Bad MAC length");
+          }
+          var a = new Uint8Array(calculated_mac);
+          var b = new Uint8Array(mac);
+          var result = 0;
+          for (var i=0; i < mac.byteLength; ++i) {
+              result = result | (a[i] ^ b[i]);
+          }
+          if (result !== 0) {
+              throw new Error("Bad MAC");
+          }
+      });
+  };
+
   //---------------------------------------------
   // Sign (or add MAC) to cihper message, 
   // then relay both chiper message and MAC.
   //---------------------------------------------
   this.signMessage = function (ciphertext) {
     var com = this._getCommitment(ciphertext);
-    return this.calcHmac(com).then(function (mac) {
+    return this._calcHmac(this.secretKey.buffer, com).then(function (mac) {
         ciphertext.mac = mac;
         return ciphertext;
     });
   };
 
-  this.calcHmac = function (data) {
+  this._calcHmac = function (keyHmac, data) {
     var crypto = window.crypto;
     if (!crypto || !crypto.subtle || typeof crypto.getRandomValues !== 'function') {
         throw new Error('WebCrypto not found');
     }
     return crypto.subtle.importKey(
-        'raw', this.secretKey.buffer, {name: 'HMAC', hash: {name: 'SHA-256'}}, false, ['sign']).
+        'raw', keyHmac, {name: 'HMAC', hash: {name: 'SHA-256'}}, false, ['sign']).
     then(function (key) {
         return crypto.subtle.sign( {name: 'HMAC', hash: 'SHA-256'}, key, data);
     });
   };
+
+  this.reportAbuse = function(sender, rcver, evidence, mac){
+    var macInput = new Uint8Array(evidence.header.byteLength + evidence.body.byteLength);
+    macInput.set(new Uint8Array(evidence.header));
+    macInput.set(new Uint8Array(evidence.body), evidence.header.byteLength);
+    return Promise.all([
+        this._verifyMAC(evidence.commitment, this.secretKey.buffer, mac, 32),
+        this._verifyMAC(macInput, evidence.commitKey, evidence.commitment, 32)
+    ]);
+  };
+
 }
 
 var server = function(){
@@ -273,19 +319,28 @@ angular.module('messengerApp', [])
       .then(function (ciphertext) {
         // draw sender...
         //TODO: ask server to sign C2
-        console.log(ciphertext);
+        // console.log(ciphertext);
         return messengerServer.signMessage(ciphertext);
       }).then(function (signedCipher) {
         console.log(signedCipher);
         return receiveMessage(senderStorage, rcverStorage, signedCipher);
       }).then(function (plaintext) {
-        return dcodeIO.ByteBuffer.wrap(plaintext.body, "utf8").toString("utf8");
+        return dcodeIO.ByteBuffer.wrap(plaintext, "utf8").toString("utf8");
       }).then(function (plaintext) {
         //TODO: check if the evidence is 
 
         //TODO: store the signed evidence for reporting in future
         console.log(plaintext);
         // draw receiver...
+      }).then(function () {
+        // Report last message
+        var history = rcverStorage.history[sender];
+        console.log(rcverStorage);
+        var evidence = history[history.length-1].evidence;
+        var mac = history[history.length-1].mac;
+        return messengerServer.reportAbuse(sender, rcver, evidence, mac).then(function (argument) {
+            console.log('reportAbuse() success');
+        });
       });
     };
     // messenger.sendOld = function(sender, rcver) {
