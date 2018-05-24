168b85a1c6dcbeeae241ad26735ba2b0398c9c31
Weikai Lin <wl572@cornell.edu>
2018-05-24 13:31:21 -0400

    comment

--- weikai/test_setup.js
+++ weikai/test_setup.js
@@ -384,11 +384,3 @@ angular.module('messengerApp', [])
     };
 
   });
-
-
-
-/*
-Copyright 2018 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
